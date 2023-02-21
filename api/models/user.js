7af4827d8274df53867b643b7d7599b713a3617f
Yeraldinnesan <ysanabria92@hotmail.com>
2023-02-21 18:13:32 -0500

    build(backEnd): :construction: Mongo+Mongoose models, schemas, relations and resolvers

---
+++ api/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdEvents: [
+    {
+      type: Schema.Types.ObjectId, //list of ids
+      ref: "Event", //relation Name of the model that relates to
+    },
+  ],
+});
+
+module.exports = mongoose.model("User", userSchema);
