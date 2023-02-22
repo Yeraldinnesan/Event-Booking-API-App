0a536396051f0331ae19d5b29d5219cb4b75f4bd
Yeraldinnesan <ysanabria92@hotmail.com>
2023-02-22 17:47:11 -0500

    feat(backEnd): :sparkles: Bookings feature

--- api/models/event.js
+++ api/models/event.js
@@ -28,3 +28,29 @@ const eventSchema = new Schema({
 });
 
 module.exports = mongoose.model("Event", eventSchema);
+//------------------------------USER
+// mutation{
+//   createUser(userInput:{firstName:"1 test user", lastName:"test", email:"1test@email.com", password:"lolo1212"}){
+//     firstName
+//     _id
+//   }
+// }
+
+// {
+//   "data": {
+//     "createUser": {
+//       "firstName": "2 test user",
+//       "_id": "63f68ec23d0339be0f773469"
+//     }
+//   }
+// }
+
+// {
+//   "data": {
+//     "createUser": {
+//       "firstName": "1 test user",
+//       "_id": "63f68e7a3d0339be0f773466"
+//     }
+//   }
+// }
+//-----------------------------------------------------------
